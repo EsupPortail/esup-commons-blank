e00203bb941eb933c08c6d4a21e5e2608b3f4d6e
llevague <lionel.levagueresse@univ-rennes1.fr>
2013-03-21 14:18:22 +0100

    Fix compiling of web-jsf-mixed

--- esup-blank-web-jsf-mixed/src/main/java/org/esupportail/blank/web/controllers/ExceptionController.java
+++ esup-blank-web-jsf-mixed/src/main/java/org/esupportail/blank/web/controllers/ExceptionController.java
@@ -69,23 +69,18 @@ public class ExceptionController extends
 
 	@Override
 	public String restart() {
-		Map<String, Object> resettables = BeanUtils
+		Map<String, Resettable> resettables = BeanUtils
 				.getBeansOfClass(Resettable.class);
 		for (String name : resettables.keySet()) {
 			if (logger.isDebugEnabled()) {
 				logger.debug("trying to reset bean [" + name + "]...");
 			}
-			Object bean = resettables.get(name);
+			Resettable bean = resettables.get(name);
 			if (bean == null) {
 				throw new ConfigException("bean [" + name + "] is null, "
 						+ "application can not be restarted.");
 			}
-			if (!(bean instanceof Resettable)) {
-				throw new ConfigException("bean [" + name
-						+ "] does not implement Resettable, "
-						+ "application can not be restarted.");
-			}
-			((Resettable) bean).reset();
+			bean.reset();
 			if (logger.isDebugEnabled()) {
 				logger.debug("bean [" + name + "] was reset.");
 			}
